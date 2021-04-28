b24663b54ee026a95b34a2e947b4d78195960aa2
Conner Vieira <connervieira@tutanota.com>
2021-04-28 12:32:17 -0400

    Added basic menu system

--- main.py
+++ main.py
@@ -9,6 +9,21 @@ from urllib.parse import urljoin, urlparse
 from urllib.error import HTTPError
 from http.client import InvalidURL
 from ssl import _create_unverified_context
+import os
+from os import system, name 
+
+
+pages_found = 0
+pages_visited = 0
+
+
+# Define the funtion that will be used to clear the screen
+def clear():
+    if name == 'nt':
+        system('cls')
+    else:
+        system('clear')
+
 
 class AnchorParser(HTMLParser):
     "Basic HTML parser that gathers a set of all href values in a webpage by targetting the anchor tag"
@@ -46,26 +61,18 @@ class AnchorParser(HTMLParser):
                 # Attribute examples: href, target, rel, etc
                 # Attribute list can be found at: https://www.w3schools.com/tags/tag_a.asp
                 if attribute == "href":
-                    # Form an absolute URL based on the relative URL
                     absoluteUrl = urljoin(self.baseURL, value)
-                    # We want to avoid href values that are not http/https
-                    # Example: <a href="mailto:person@some.com">Send Email Now!</a>
-                    if urlparse(absoluteUrl).scheme in ["http", "https"]:
-                        # Once a new hyperlink is obtained, add it to the set
-                        self.pageLinks.add(absoluteUrl)
+                    if urlparse(absoluteUrl).scheme in ["http", "https"]: # Only follow links that use HTTP or HTTPS
+                        if (urlparse(site_to_test).netloc in str(absoluteUrl)): # Only follow links on the same domain as the initial page
+                            global pages_found
+                            pages_found = pages_found + 1
+                            self.pageLinks.add(absoluteUrl)
 
 
 class MyWebCrawler(object):
     "Basic Web Crawler using only Python Standard Libraries"
 
     def __init__(self, url, maxCrawl=10):
-        """Constructor for MyWebCrawler
-        Args:
-            url (str): The starting URL for the web crawler
-            maxCrawl (int): Max amount of URLs to crawl
-        Returns:
-            None
-        """
         self.visited = set() # To track all visited urls
         self.starterUrl = url
         self.max = maxCrawl
@@ -79,27 +86,25 @@ class MyWebCrawler(object):
         urlsToParse = {self.starterUrl}
         # While there are still more URLs to parse and we have not exceeded the crawl limit
         while(len(urlsToParse) > 0 and len(self.visited) < self.max):
-            # Get the next URL to visit and remove it the set
+            # Get the next URL to visit and remove it from the set
             nextUrl = urlsToParse.pop()
             # Skip the next URL if it has already been visited
+            global pages_visited
+            global pages_found
+            pages_visited = pages_visited + 1
+            clear()
+            print("Pages visited: " + str(pages_visited))
+            print("Pages found: " + str(pages_found))
             if nextUrl not in self.visited:
                 # Mark the next URL as visited
                 self.visited.add(nextUrl)
                 # Call the .parse method to make a web request
                 # and parse any new URLs from the HTML content
                 # any new URLs found will be appended to the urlsToParse set
-                print("Parsing: {}".format(nextUrl))
+                # print("Parsing: {}".format(nextUrl))
                 urlsToParse |= self.parse(nextUrl)
 
     def parse(self, url):
-        """Makes a web request and uses an AnchorParser object to parse the HTML content
-        Args:
-            url (str): URL to request and parse
-        Returns:
-            set: Absolute URLs found in HTML content
-        Exceptions Caught:
-            HTTPError, InvalidURL, UnicodeDecodeError
-        """
         try:
             # Open the URL, read content, decode content
             htmlContent = urlopen(url, context=_create_unverified_context()).read().decode()
@@ -110,9 +115,7 @@ class MyWebCrawler(object):
             # The AnchorParser object has a set of absolute URLs that can be returned
             return parser.getLinks()
         except (HTTPError, InvalidURL, UnicodeDecodeError):
-            # In the case we get any http error
-            # Example: 403 code
-            print("FAILED: {}".format(url))
+            # In the case we get any HTTP error
             return set()
 
     def getVisited(self):
@@ -123,9 +126,34 @@ class MyWebCrawler(object):
         """
         return self.visited
 
+site_to_test = input("Please enter a site to crawl: ")
+max_crawl = input("Please enter a maximum number of pages to crawl: ")
 
-if __name__ == "__main__":
-    # Change the URL below
-    crawler = MyWebCrawler("https://CHANGEME.com", maxCrawl=20)
-    crawler.crawl()
-    print("\nThe following sites were visited:\n{}".format(crawler.getVisited()))
+crawler = MyWebCrawler(site_to_test, maxCrawl=int(max_crawl))
+
+print("Crawling site")
+
+crawler.crawl()
+
+
+while True:
+    clear()
+
+    print("Crawl complete. What would you like to do next?")
+    print("1. View visited domains")
+    print("2. View discovered domains")
+    print("3. View statistics")
+    selection = int(input("Selection: "))
+
+    clear()
+    if (selection == 1):
+        print("The following sites were visited:\n{}".format(crawler.getVisited()))
+    elif (selection == 2):
+        print("Sorry, this feature has not yet been implemented.")
+    elif (selection == 3):
+        print("Pages visited: " + str(pages_visited))
+        print("Pages found: " + str(pages_found))
+    else:
+        print("Error: Invalid selection")
+    
+    input("") # Wait for the user to press enter before continuing
